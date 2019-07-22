631ec63d9dcc6eec68a25baa8135c02b3622f20b
Mika Sormunen <sormmi@gmail.com>
2019-07-22 18:17:56 +0300

    Express server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+// Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT | 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
